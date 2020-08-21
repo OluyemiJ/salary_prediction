bb67823ede40968a357ac776db782a8e4276579b
Oluyemi Jegede <oluyemijegede@Oluyemis-MacBook.local>
2020-08-21 18:26:09 +0100

    added scraper and readme

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
-
-# Press ⇧F10 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+import glassdoor_scraper as gs
+import pandas as pd
+path = '/Users/oluyemijegede/Downloads/chromedriver'
+df = gs.get_jobs('data scientist', 5, False, path, 15)
